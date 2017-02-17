903be3ec4c515b266cafeb1000b678c3ee6127b1
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2017-02-17 14:03:09 -0600

    Add a new activity for the roulette game board.
    The goal here was to setup the layouts and a couple of simple widgets to achieve the look of a roulette game board. In the component tree view of Android Studio, you can see how different layouts have been nested to achieve the roulette board layout. The widgets themselves can be modified later on when we have established which ones would serve best to achieve the look and feel we need.

---
+++ app/src/main/java/com/mouthofrandom/cardshark/RouletteBoardActivity.java
@@ -0,0 +1,13 @@
+package com.mouthofrandom.cardshark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RouletteBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roulette_board);
+    }
+}

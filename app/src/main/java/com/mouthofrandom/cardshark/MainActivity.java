aff2d8ff1702a1aca7e319309c909d8b0d8a859e
Tyler Woodfin <tylerjwoodfin@gmail.com>
2017-02-18 15:43:22 -0600

    Added Currency, 8-bit toolbar
    There are getters and setters in the main activity that you can use to
    update the balance. On first launch, the user has $2,000. I’ve included
    several 8-bit fonts you can use across the app.

--- app/src/main/java/com/mouthofrandom/cardshark/MainActivity.java
+++ app/src/main/java/com/mouthofrandom/cardshark/MainActivity.java
@@ -1,5 +1,9 @@
 package com.mouthofrandom.cardshark;
 
+import android.app.ActionBar;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +12,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);   //set fullscreen
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //Update Status Bar with current balance
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+
+        Typeface status_font = Typeface.createFromAsset(getAssets(),  "fonts/8bitOperatorPlusSC-Regular.ttf");
+        TextView status_text = (TextView)findViewById(R.id.statustext);
+        status_text.setTypeface(status_font);
+        status_text.setText(formatter.format(getBalance()));
     }
 
     @Override
@@ -48,5 +65,71 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Save data as an internal text file. Can be replaced in a future iteration.
+    public void saveData(String filename, String contents)
+    {
+        try
+        {
+            FileOutputStream fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(contents.getBytes());
+            fos.close();
+            //toast("Saved Successfully under Title: " + filename);
+        }
+        catch(Exception e)
+        {
+            toast("There was a problem saving.");
+        }
+    }
+
+    //Save new Balance. Parameter replaces old balance.
+    public void setBalance(double balance)
+    {
+        saveData("balance.csc", Double.toString(balance));
+    }
+
+    //Display a toast message
+    public void toast(String message)
+    {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    //Load Balance
+    public double getBalance()
+    {
+
+        String filename = "balance.csc";
+
+        try
+        {
+            FileInputStream fileIn=openFileInput(filename);
+            InputStreamReader InputRead= new InputStreamReader(fileIn);
+
+            char[] inputBuffer= new char[100];
+            String s="";
+            int charRead;
+
+            while ((charRead=InputRead.read(inputBuffer))>0)
+            {
+                // char to string conversion
+                String string=String.copyValueOf(inputBuffer,0,charRead);
+                s +=string;
+            }
+            InputRead.close();
+            // toast(filename + " read successfully");
+            return Double.parseDouble(s);
+        }
+        catch(FileNotFoundException e)
+        {
+            setBalance(0);
+            return 2000;
+        }
+        catch(IOException e)
+        {
+            toast("IOException: " + filename);
+        }
+        return -1;
     }
 }

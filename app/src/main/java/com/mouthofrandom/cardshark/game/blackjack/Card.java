1cb2c4f7057652e999d3027c38b7c5afe5f96539
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2017-03-24 23:15:35 -0500

    Add a class for representing playing cards.

---
+++ app/src/main/java/com/mouthofrandom/cardshark/game/blackjack/Card.java
@@ -0,0 +1,40 @@
+// ======================================================================
+// Filename: Card.java
+// Description: Define and implement a basic playing card.
+// Created: Fri Mar 24 21:37:59 2017 (-0500)
+// Last-Updated: Fri Mar 24 23:02:27 2017 (-0500)
+// Author: Ivan Guerra <Ivan.E.Guerra-1@ou.edu>
+// ======================================================================
+package com.mouthofrandom.cardshark.game.blackjack;
+
+public class Card {
+    private String suit;
+    private String cardType;
+    private int value;
+
+    public Card(final String suit, final String cardType, final int value) {
+	this.suit = suit;
+	this.cardType = cardType;
+	this.value = value;
+    }
+
+    /* Return the card suit. */
+    public String getSuit() {
+	return suit;
+    }
+
+    /* Return the card type. */
+    public String getCardType() {
+	return cardType;
+    }
+
+    /* Return the card value. */
+    public int getValue() {
+	return value;
+    }
+
+    /* Return the string representation of a Card. */
+    public String toString() {
+	return ("(" + this.suit + ", " + this.cardType + ", " + this.value + ")");
+    }
+}

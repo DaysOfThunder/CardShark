6ec65aebe43f6486bd33b037aaabc6221b472e5b
Elliot Eudaley <Elliot.M.Eudaley-1@ou.edu>
2017-03-28 19:34:59 -0500

    Added png animation images along with an xml containg the animation. I've successfully run the app with the animation playing as the title screen.

--- app/src/main/java/com/mouthofrandom/cardshark/activities/TitleScreenActivity.java
+++ app/src/main/java/com/mouthofrandom/cardshark/activities/TitleScreenActivity.java
@@ -1,22 +1,35 @@
 package com.mouthofrandom.cardshark.activities;
 
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.mouthofrandom.cardshark.R;
 
 /**
  * Created by Elliot on 3/27/2017.
  */
-import com.mouthofrandom.cardshark.R;
+
+
 
 public class TitleScreenActivity extends AppCompatActivity{
 
+    AnimationDrawable titleAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
 
+        //get the image view from the activity_title_screen layout and add the background resource then begin animation
+        ImageView sharksplash = (ImageView) findViewById(R.id.sharksplash);
+        sharksplash.setBackgroundResource(R.drawable.title_animation);
+        titleAnimation = (AnimationDrawable) sharksplash.getBackground();
+        titleAnimation.start();
 
 
 
